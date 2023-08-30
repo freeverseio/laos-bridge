cf8294a9315bad7e24cb26b7c750ab682ee67e23
Dastan <dastan@freeverse.io>
2023-08-30 11:05:12 +0300

    Add rococo evochain bridge

---
+++ relays/laos-relay/src/bridges/rococo_evochain/rococo_headers_to_evochain.rs
@@ -0,0 +1,38 @@
+//! Rococo-to-Evochain headers sync entrypoint.
+
+use crate::cli::bridge::{CliBridgeBase, RelayToRelayHeadersCliBridge};
+use substrate_relay_helper::{
+    finality::{DirectSubmitGrandpaFinalityProofCallBuilder, SubstrateFinalitySyncPipeline},
+    finality_base::{engine::Grandpa as GrandpaFinalityEngine, SubstrateFinalityPipeline},
+};
+
+/// Description of Rococo -> Evochain finalized headers bridge.
+#[derive(Clone, Debug)]
+pub struct RococoFinalityToEvochain;
+
+impl SubstrateFinalityPipeline for RococoFinalityToEvochain {
+    type SourceChain = relay_rococo_client::Rococo;
+    type TargetChain = relay_laos_evolution_client::Evochain;
+
+    type FinalityEngine = GrandpaFinalityEngine<Self::SourceChain>;
+}
+
+impl SubstrateFinalitySyncPipeline for RococoFinalityToEvochain {
+    type SubmitFinalityProofCallBuilder = DirectSubmitGrandpaFinalityProofCallBuilder<
+        Self,
+        node_template_runtime::Runtime,
+        node_template_runtime::RococoGrandpaInstance,
+    >;
+}
+
+/// `Rialto` to `Millau` bridge definition.
+pub struct RococoToEvochainCliBridge {}
+
+impl CliBridgeBase for RococoToEvochainCliBridge {
+    type Source = relay_rococo_client::Rococo;
+    type Target = relay_laos_evolution_client::Evochain;
+}
+
+impl RelayToRelayHeadersCliBridge for RococoToEvochainCliBridge {
+    type Finality = RococoFinalityToEvochain;
+}
