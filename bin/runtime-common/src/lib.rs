2038e58d5ccea0c77b9b1a4cacf2a433604ad415
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-09-01 10:56:40 +0200

    fmt

--- bin/runtime-common/src/lib.rs
+++ bin/runtime-common/src/lib.rs
@@ -25,17 +25,16 @@
 // use sp_runtime::transaction_validity::TransactionValidity;
 use xcm::v3::NetworkId;
 
-
 /// A mapping over `NetworkId`.
 /// Since `NetworkId` doesn't include `Evochain`, `Rococo` and `OwnershipParachain`, we create some
 /// synthetic associations between these chains and `NetworkId` chains.
 pub enum CustomNetworkId {
-	/// The Evochain network ID, associated with Kusama.
-	Evochain,
-	/// The Rococo network ID, associated with Polkadot.
-	Rococo,
-	/// The OwnershipParachain network ID, associated with Westend.
-	OwnershipParachain,
+    /// The Evochain network ID, associated with Kusama.
+    Evochain,
+    /// The Rococo network ID, associated with Polkadot.
+    Rococo,
+    /// The OwnershipParachain network ID, associated with Westend.
+    OwnershipParachain,
 }
 
 // impl TryFrom<bp_runtime::ChainId> for CustomNetworkId {
@@ -57,12 +56,12 @@ pub enum CustomNetworkId {
 // }
 
 impl CustomNetworkId {
-	/// Converts self to XCM' network id.
-	pub const fn as_network_id(&self) -> NetworkId {
-		match *self {
-			CustomNetworkId::Evochain => NetworkId::Kusama,
-			CustomNetworkId::Rococo => NetworkId::Polkadot,
-			CustomNetworkId::OwnershipParachain => NetworkId::Westend,
-		}
-	}
+    /// Converts self to XCM' network id.
+    pub const fn as_network_id(&self) -> NetworkId {
+        match *self {
+            CustomNetworkId::Evochain => NetworkId::Kusama,
+            CustomNetworkId::Rococo => NetworkId::Polkadot,
+            CustomNetworkId::OwnershipParachain => NetworkId::Westend,
+        }
+    }
 }
